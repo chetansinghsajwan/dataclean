d2546b7dd5995ba12bfbbcf3d0143110c4c5b1e1
Chetan Singh Sajwan <chetanss@jmangroup.com>
2026-09-02 11:05:48 +0530

    gender cleaner inherit enum clenaer

--- src/dataclean/cleaners/gender_cleaner.py
+++ src/dataclean/cleaners/gender_cleaner.py
@@ -3,8 +3,9 @@ from collections.abc import Iterable, Mapping
 from enum import StrEnum
 
 from dataclean.types import checked
+from dataclean.utils.case import TextCase, convert_to_case
 
-from .enum_cleaner import EnumCleaner
+from .enum_cleaner import EnumCleaner, EnumCleanerMatcher
 
 _logger = logging.getLogger(__name__)
 
@@ -18,6 +19,12 @@ class GenderCleaner(EnumCleaner):
 
     out_format: Format = Format.FULL
 
+    _FORMATS = {
+        "male": {Format.FULL: "male", Format.CHAR: "m", Format.BINARY: "1"},
+        "female": {Format.FULL: "female", Format.CHAR: "f", Format.BINARY: "0"},
+        "other": {Format.FULL: "other", Format.CHAR: "o", Format.BINARY: "-1"},
+    }
+
     DEFAULT_CLEANER_MATCHING_WORDS = ["gender", "sex"]
     DEFAULT_FUZZY_THRESHOLD = 0.9
 
@@ -29,6 +36,8 @@ class GenderCleaner(EnumCleaner):
 
     def __init__(
         self,
+        format: Format = Format.FULL,
+        case: TextCase = TextCase.PASCAL,
         genders: Mapping[str, Iterable[str]] = GENDERS,
         extra_genders: Mapping[str, Iterable[str]] = {},
         tags: tuple[str, ...] = (),
@@ -38,7 +47,13 @@ class GenderCleaner(EnumCleaner):
         self._match_words = frozenset(cleaner_matching_words)
 
         _logger.info("Building cases for gender cleaner...")
-        cases = self._build_cases(genders, extra_genders, fuzzy_threshold)
+        cases = self._build_cases(
+            genders=genders,
+            extra_genders=extra_genders,
+            fuzzy_threshold=fuzzy_threshold,
+            format=format,
+            case=case,
+        )
 
         super().__init__(
             cases=cases,
@@ -51,14 +66,37 @@ class GenderCleaner(EnumCleaner):
         genders: Mapping[str, Iterable[str]],
         extra_genders: Mapping[str, Iterable[str]],
         fuzzy_threshold: float,
-    ) -> dict[str, EnumCleaner.Matcher]:
+        format: Format,
+        case: TextCase,
+    ) -> dict[str, EnumCleanerMatcher]:
         all_genders = {**genders, **extra_genders}
 
-        cases: dict[str, EnumCleaner.Matcher] = {
-            gender: EnumCleaner.FuzzyMatcher(
+        print(all_genders)
+
+        all_genders = {
+            (
+                GenderCleaner._FORMATS[n][format]
+                if n in GenderCleaner._FORMATS.keys()
+                else n
+            ): variants
+            for n, variants in all_genders.items()
+        }
+
+        print(all_genders)
+
+        if format != GenderCleaner.Format.BINARY:
+            all_genders = {
+                convert_to_case(n, case): variants
+                for n, variants in all_genders.items()
+            }
+
+        print(all_genders)
+
+        cases: dict[str, EnumCleanerMatcher] = {
+            gender: EnumCleaner.ExactMatcher(
                 variants=variants,
                 case_sensitive=False,
-                threshold=fuzzy_threshold,
+                # threshold=fuzzy_threshold,
             )
             for gender, variants in all_genders.items()
         }
