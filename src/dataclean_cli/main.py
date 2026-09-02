641db2ed68caa0da462481097f81bc85ea4a82be
Chetan Singh Sajwan <chetanss@jmangroup.com>
2026-09-02 18:22:32 +0530

    wip: impl cli clean

--- src/dataclean_cli/main.py
+++ src/dataclean_cli/main.py
@@ -1,5 +1,9 @@
 import typer
 
+import dataclean
+
+from . import logs
+
 app = typer.Typer()
 
 
@@ -15,7 +19,7 @@ def parse_mapping(value: str) -> tuple[str, str]:
 @app.command()
 def clean(
     paths: list[str],
-    write_path: str | None = None,
+    write_path: str,
     catalog: str | None = None,
     clean_cols: bool = True,
     auto_rename_cols: bool = True,
@@ -24,15 +28,30 @@ def clean(
     inplace: bool | None = None,
     cleaners: list[str] | None = None,
     dry_run: bool = False,
+    log: logs.LevelNames = logs.defaultLevel,
 ):
     """Clean the data."""
 
-    # use_global_config: bool = True
-    # parsed_rename_col_map = (
-    #     dict(parse_mapping(x) for x in rename_col) if rename_col else {}
-    # )
+    logs.setup_logging(log)
 
-    pass
+    use_global_config: bool = True
+    parsed_rename_col_map = (
+        dict(parse_mapping(x) for x in rename_col) if rename_col else {}
+    )
+
+    dataclean.clean_paths(
+        paths=paths,
+        write_path=write_path,
+        catalog=catalog,
+        clean_cols=clean_cols,
+        rename_cols=auto_rename_cols,
+        rename_col_map=parsed_rename_col_map,
+        ignore_cols=ignore_col,
+        inplace=inplace,
+        use_global_config=use_global_config,
+        cleaners=cleaners,
+        dry_run=dry_run,
+    )
 
 
 def main():
